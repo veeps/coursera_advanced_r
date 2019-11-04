fc16b1f84763f96c60d61c5570b49f16f938836e
Vivian Peng <vivianpeng@vivians-mbp.home>
2019-11-03 21:01:32 -0800

    factorial loop and reduce functions

---
+++ factorial_code.R
@@ -0,0 +1,34 @@
+# n * (n-1) * (n - 2) * … * 1
+
+
+
+
+# Factorial_loop: a version that computes the factorial of an integer using looping (such as a for loop)
+factorial_loop <- function(n){
+  if (n ==0){
+    1
+  }else{
+    y <- 1
+    for(i in 1:n){
+      y <-y*i
+    }
+   y
+  }
+}
+
+
+# Factorial_reduce: a version that computes the factorial using the reduce() function in the purrr package. Alternatively, you can use the Reduce() function in the base package.
+library(purrr)
+
+factorial_reduce <- function(n){
+  reduce(c(1:n), function(x, y){
+  x*y
+})
+}
+
+
+# Factorial_func: a version that uses recursion to compute the factorial.
+# Factorial_mem: a version that uses memoization to compute the factorial.
+
+
+

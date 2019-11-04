aa1092d50d6b843d2e21234ce1b3ad402b5d3ac2
Vivian Peng <vivianpeng@vivians-mbp.home>
2019-11-03 21:56:49 -0800

    factorial functions recursive annd memoization

--- factorial_code.R
+++ factorial_code.R
@@ -4,7 +4,7 @@
 
 
 # Factorial_loop: a version that computes the factorial of an integer using looping (such as a for loop)
-factorial_loop <- function(n){
+Factorial_loop <- function(n){
   if (n ==0){
     1
   }else{
@@ -20,7 +20,7 @@ factorial_loop <- function(n){
 # Factorial_reduce: a version that computes the factorial using the reduce() function in the purrr package. Alternatively, you can use the Reduce() function in the base package.
 library(purrr)
 
-factorial_reduce <- function(n){
+Factorial_reduce <- function(n){
   if(n==0){
     1
   }else{
@@ -30,11 +30,30 @@ factorial_reduce <- function(n){
   }
 }
 
-factorial_reduce(0)
-
+Factorial_reduce(0)
 
 # Factorial_func: a version that uses recursion to compute the factorial.
+Factorial_func <- function(x){
+  if(x==0){
+    1
+  } else{
+    x*Factorial_func(x-1)
+  }
+}
+
+Factorial_func(5)
+
 # Factorial_mem: a version that uses memoization to compute the factorial.
+Factorial_mem <- function(n){
+  stopifnot(n > 0)
+  
+  if(!is.na(Factorial_tbl[n])){
+    Factorial_tbl[n]
+  } else{
+    Factorial_tbl[n-1] <<- Factorial_mem(n-1)
+    n*Factorial_tbl[n-1]
+  }
+}
 
-
-
+Factorial_mem(10)
+Factorial_func(10)
